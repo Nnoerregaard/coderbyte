5f3f1b5ceac6224ab76e7dd70eccc8f116cc2579
Niklas Moss <nin@envidan.dk>
2021-02-09 06:02:18 +0100

    Added axios and a quick request to test it out. Of course, the API key should not be in source control but time is limited

--- giphy-web/src/image-explorer/ResultSection.tsx
+++ giphy-web/src/image-explorer/ResultSection.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Image from './Image';
+import axios from 'axios'
 
 function ResultSection() {
+  useEffect(() => {
+    // TODO: Move out into configuration, and lift state when we get to the query section
+    axios.get('https://api.giphy.com/v1/stickers/search', { params: { q: "cat", limit: 10, rating: "g", api_key: "1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq" }} ).then(result => {
+      console.log(result)
+    }).catch(error => {
+      console.log(error)
+    })
+  })
   // TODO: When you get to the data access, iterate over the result from the endpoint
   return (
     <Container> 
